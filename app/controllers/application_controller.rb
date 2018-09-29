20c7be0ad2e8b11af04dac8bdae446cd94587342
Yurina Shin <yurinas@gmail.com>
2018-09-29 23:12:39 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -30,7 +30,8 @@ class ApplicationController < Sinatra::Base
 
   post '/sessions' do
     @user = User.find_by(email: params["email"], password: params["password"])
-    if session[:id] = @user.id && params[:password] == @user.password
+    if params[:password] == @user.password
+      session[:id] = @user.id
       redirect '/users/home'
     else
       redirect '/sessions/login'
